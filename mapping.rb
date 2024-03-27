a5f7f1a0f234a38e34b24b006afa9e814760ed96
Josh Cutts <josh.cutts1@gmail.com>
2024-03-27 15:54:43 -0700

    add destination range start

--- mapping.rb
+++ mapping.rb
@@ -1,4 +1,9 @@
 class Mapping
   def initialize(raw_mapping)
+    @destination_range_start = raw_mapping.split[0].to_i
+  end
+
+  def destination_range_start
+    @destination_range_start
   end
 end
--- mapping_spec.rb
+++ mapping_spec.rb
@@ -3,6 +3,7 @@ require_relative 'mapping'
 describe Mapping do
   it 'works' do
     mapping = Mapping.new("50 98 2")
+    expect(mapping.destination_range_start).to eq(50)
   end
 end
 
